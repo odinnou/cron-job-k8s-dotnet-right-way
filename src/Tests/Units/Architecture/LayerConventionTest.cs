41b0f702229f20643086f86b35e52151776e7dbb
Rémi DESPELCHIN <rdespelchin@gmail.com>
2023-03-10 08:44:35 +0100

    test(clean): remove useless tests

--- src/Tests/Units/Architecture/LayerConventionTest.cs
+++ src/Tests/Units/Architecture/LayerConventionTest.cs
@@ -8,28 +8,13 @@ namespace Tests.Units.Architecture;
 
 public class LayerConventionTest : BaseArchitectureTest
 {
-    private readonly IObjectProvider<IType> _drivingAdaptersLayer = Types().That().ResideInNamespace("Runner.DrivingAdapters.*", useRegularExpressions: true).As("Driving adapters layer");
     private readonly IObjectProvider<IType> _coreLayer = Types().That().ResideInNamespace("Runner.Core.*", useRegularExpressions: true).As("Core layer");
     private readonly IObjectProvider<IType> _drivenAdaptersLayer = Types().That().ResideInNamespace("Runner.DrivenAdapters.*", useRegularExpressions: true).As("Driven adapters layer");
-
-    [Fact]
-    public void Types_that_resides_in_Driving_adapters_layer_should_not_depend_on_any_types_that_reside_in_Driven_adapters_layer()
-    {
-        // arrange act assert
-        Types().That().Are(_drivingAdaptersLayer).Should().NotDependOnAny(_drivenAdaptersLayer).Because("it's forbidden").Check(Architecture);
-    }
 
     [Fact]
     public void Types_that_resides_in_Core_layer_should_not_depend_on_any_types_that_reside_in_Driven_adapters_layer_and_not_depend_on_any_types_that_reside_in_Driving_adapters_layer()
     {
         // arrange act assert
-        Types().That().Are(_coreLayer).Should().NotDependOnAny(_drivingAdaptersLayer).AndShould().NotDependOnAny(_drivenAdaptersLayer).Because("it's forbidden").Check(Architecture);
-    }
-
-    [Fact]
-    public void Types_that_resides_in_Driven_adapters_layer_should_not_depend_on_any_types_that_reside_in_Driving_adapters_layer()
-    {
-        // arrange act assert
-        Types().That().Are(_drivenAdaptersLayer).Should().NotDependOnAny(_drivingAdaptersLayer).Because("it's forbidden").Check(Architecture);
+        Types().That().Are(_coreLayer).Should().NotDependOnAny(_drivenAdaptersLayer).Because("it's forbidden").Check(Architecture);
     }
 }
--- src/Tests/Units/Architecture/NamingConventionTest.cs
+++ src/Tests/Units/Architecture/NamingConventionTest.cs
@@ -8,30 +8,9 @@ namespace Tests.Units.Architecture;
 public class NamingConventionTest : BaseArchitectureTest
 {
     [Fact]
-    public void Classes_that_assignable_to_Profile_should_have_name_ending_with_MappingProfile()
-    {
-        // arrange act assert
-        Classes().That().AreAssignableTo("AutoMapper.Profile").Should().HaveNameEndingWith("MappingProfile").Check(Architecture);
-    }
-
-    [Fact]
-    public void Classes_that_resides_in_DatabaseAdapters_Entities_namespace_should_have_name_ending_with_Entity()
-    {
-        // arrange act assert
-        Classes().That().ResideInNamespace("Runner.DrivenAdapters.DatabaseAdapters.Entities").And().DoNotResideInNamespace("Runner.DrivenAdapters.DatabaseAdapters.Entities.Mappings").Should().HaveNameEndingWith("Entity").Check(Architecture);
-    }
-
-    [Fact]
     public void Interfaces_that_resides_in_Core_Ports_namespace_should_have_name_ending_with_Port()
     {
         // arrange act assert
         Interfaces().That().ResideInNamespace("Runner.Core.Ports").Should().HaveNameEndingWith("Port").Check(Architecture);
-    }
-
-    [Fact]
-    public void Classes_that_assignable_to_System_Exception_should_have_name_ending_with_Exception()
-    {
-        // arrange act assert
-        Classes().That().AreAssignableTo("System.Exception").Should().HaveNameEndingWith("Exception").Check(Architecture);
     }
 }
